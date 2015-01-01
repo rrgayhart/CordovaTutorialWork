e1d2ee16e2f461134c06568275e6dd26bef6c454
Meeka <rrgayhart@gmail.com>
2015-01-01 14:53:20 -0600

    Adds native notification

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -12,7 +12,18 @@
     $('.help-btn').on('click', function() {
         alert("Employee Directory v3.4");
     });
-
+    document.addEventListener('deviceready', function () {
+      if (navigator.notification) { // Override default HTML alert with native dialog
+        window.alert = function (message) {
+          navigator.notification.alert(
+            message,    // message
+            null,       // callback
+            "Workshop", // title
+            'OK'        // buttonName
+          );
+        };
+      }
+    }, false);
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
         service.findByName($('.search-key').val()).done(function (employees) {
